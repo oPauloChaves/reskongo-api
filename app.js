e2f5ad0df7c43c07059c64593d98c9eec653fa5e
Paulo Chaves <paulochavesbr@gmail.com>
2016-11-06 15:05:33 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const compress = require('compression')
+const methodOverride = require('method-override')
+const cors = require('cors')
+const config = require('./config')
+const routes = require('./routes')
+
+const app = express()
+
+if (config.env === 'development') {
+  app.use(logger('dev'))
+}
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cookieParser())
+app.use(compress())
+app.use(methodOverride());
+
+app.use(cors())
+
+// mount all routes on /api path
+app.use('/api', routes)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (config.env === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({
+      error: {
+        message: err.message,
+        error: err
+      }
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
+    error: {
+      message: err.message,
+      error: {}
+    }
+  })
+})
+
+module.exports = app
---
+++ config/index.js
@@ -0,0 +1,29 @@
+/**
+ * Generate secret key: https://www.grc.com/passwords.htm
+ */
+
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+
+const defaultConfig = {
+  root: process.cwd(),
+  env: 'production',
+  jwtScret: 'xCLjgRFBunBugeTjo2N4BOVkdisHGMmnBfBGrPvqxEFuzD6VWg7trCesdQvEN0w',
+  port: 3000,
+  db: 'mongodb://localhost/reskongo'
+}
+
+const config = {
+  production: {},
+  development: {
+    env: 'development',
+    db: 'mongodb://localhost/reskongo-dev'
+  },
+  test: {
+    db: 'mongodb://localhost/reskongo-test',
+    env: 'test'
+  }
+}
+
+module.exports = Object.assign(defaultConfig, config[env]);
